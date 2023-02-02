69667b6ce85bb67d66cc553c6f8f6d1160a2de8e
aonghus <aonghuslawlor@gmail.com>
2023-02-02 17:43:59 +0000

    interfaces

---
+++ src/project20280/interfaces/Entry.java
@@ -0,0 +1,21 @@
+package project20280.interfaces;
+
+/**
+ * Interface for a key-value pair.
+ */
+public interface Entry<K, V> {
+    /**
+     * Returns the key stored in this entry.
+     *
+     * @return the entry's key
+     */
+    K getKey();
+
+    /**
+     * Returns the value stored in this entry.
+     *
+     * @return the entry's value
+     */
+    V getValue();
+}
+
